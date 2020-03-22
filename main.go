e9d16435ff8f3335f6fbe9633db7231e55b89c62
THEToilet <bp18015@shibaura-it.ac.jp>
2020-03-23 02:43:18 +0900

    :tada: Add main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	// handle static assets
+	mux := http.NewServeMux()
+
+	// starting up the server
+	server := &http.Server{
+		Addr:           config.Address,
+		Handler:        mux,
+		ReadTimeout:    time.Duration(config.ReadTimeout * int64(time.Second)),
+		WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
+		MaxHeaderBytes: 1 << 20,
+	}
+	server.ListenAndServe()
+}
