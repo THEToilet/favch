8133ea6a57298c3c7695868445267cb653f6ee2c
THEToilet <bp18015@shibaura-it.ac.jp>
2020-03-23 17:35:56 +0900

    :sparkles: Copy main.go

--- main.go
+++ main.go
@@ -10,6 +10,33 @@ func main() {
 	// handle static assets
 	mux := http.NewServeMux()
 
+	files := http.FileServer(http.Dir(config.Static))
+	// Deleate Prefix
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	//
+	// all route patterns matched here
+	// route handler functions defined in other files
+	//
+
+	// index
+	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
+
+	// defined in route_auth.go
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	// defined in route_thread.go
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
 	// starting up the server
 	server := &http.Server{
 		Addr:           config.Address,
